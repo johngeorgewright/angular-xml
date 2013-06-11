cba68cd5c0c657d0db5cc331bfa624b62b58c088
John Wright <johngeorge.wright@gmail.com>
2013-06-11 21:20:33 +0100

    Synta checking, unit testing and autocompiling when commiting the js file

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env sh
-grunt
-exit $?
+
+command()
+{
+  $@
+  code=$?
+  if [ $code -gt 0 ]
+  then
+    exit $code
+  fi
+}
+
+changed=`git diff --name-only | grep angular-xml.js`
+
+if [ $changed ]
+then
+  command npm run-script jshint
+  command npm run-script karma
+  command npm run-script compile
+  git add angular-xml.min.js
+fi
 
