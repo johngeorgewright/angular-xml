6f0d4fbb9b7502bf128db054457dc6d0957764e3
John Wright <johngeorge.wright@gmail.com>
2013-08-21 13:51:25 +0100

    Updated karma configuration file for use with latest version

--- test/karma.js
+++ test/karma.js
@@ -1,71 +1,77 @@
-/* jshint undef: false */
+/* global module */
 
-// Karma configuration
-// Generated on Sat May 18 2013 18:20:34 GMT+0100 (BST)
+module.exports = function (config) {
 
+  config.set({
 
-// base path, that will be used to resolve files and exclude
-basePath = '..';
-
-
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'angular-xml.js',
-  'test/**/*Spec.js'
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '..',
 
 
-// list of files to exclude
-exclude = [
-  '**/*.swp'
-];
+    frameworks: ['jasmine'],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'angular-xml.js',
+      'test/**/*Spec.js'
+    ],
 
 
-// web server port
-port = 9876;
+    // list of files to exclude
+    exclude: [
+      '**/*.swp'
+    ],
 
 
-// cli runner port
-runnerPort = 9100;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit'
+    reporters: ['progress'],
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // web server port
+    port: 9876,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // cli runner port
+    runnerPort: 9100,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+  });
+
+};
+
