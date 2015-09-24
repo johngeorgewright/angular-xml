cd8fe0e52ff84541878feee87e7a51e301270ecd
John Wright <johngeorge.wright@gmail.com>
2015-09-24 13:50:17 +0100

    Seperating tests.

--- test/e2e/xmlSpec.js
+++ test/e2e/xmlSpec.js
@@ -4,8 +4,11 @@ describe('x2js', function () {
     browser.get('/test/e2e/xmlSpec.html');
   });
 
-  it('will render 2 blogs and display configuration title', function () {
+  it('will render 2 blogs', function () {
     expect(element.all(by.repeater('blog in blogs')).count()).toBe(2);
+  });
+
+  it('display configuration title', function () {
     expect(element(by.binding('title')).getText()).toEqual('login required for myapp');
   });
 
