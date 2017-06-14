dc24508409660bd6a12fef42b07f39edf822bc6f
John Wright <johngeorge.wright@gmail.com>
2017-06-14 23:11:07 +0100

    patch: try use capabillities

--- test/e2e.travis.conf.js
+++ test/e2e.travis.conf.js
@@ -5,5 +5,8 @@ exports.config = {
   specs: ['e2e/**/*Spec.js'],
   baseUrl: 'http://localhost:8000',
   sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  capabilities: {
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+  }
 };
